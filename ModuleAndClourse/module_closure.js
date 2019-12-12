49aa1bca0821f0b21acad1346b4d367fb92ad4cc
Komakech Godfrey Awich <koma92awich@gmail.com>
2019-12-11 21:24:30 -0600

    closure

--- ModuleAndClourse/module_closure.js
+++ ModuleAndClourse/module_closure.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+window.onload = function () {
 var timer = null;// stores ID of the interval timer
 
 var rudyTimer = (function (){
@@ -24,29 +24,94 @@ var rudyTimer = (function (){
     } );
 
 
-var accountDetails = (function(){
-    
-return {
-    
-    accountCreated: function(accName,deposit ){
-        this.accName = accName;
-        this.deposit = deposit;
-    },
-    
-    print:function (){
-        return "Account Name :" +this.accName + "  Balace : " + this.deposit;
-    }
-    
-    
-} ;   
-    
+//var accountDetails = (function(){
+//    
+//return {
+//    
+//    accountCreated: function(accName,deposit ){
+//        this.accName = accName;
+//        this.deposit = deposit;
+//    },
+//    
+//    print:function (){
+//        return "Account Name :" +this.accName + "  Balace : " + this.deposit;
+//    }
+//    
+//    
+//} ;   
+//    
+//});
+//
+//
+//
+//windows.onload = function(){
+//    
+//    var form = document.getElementById("form");
+//    var textArea = document.getElementById("textArea");
+//    var accountInfoList = [];
+//    var addAcc;
+//    
+//    form.onsubmit = submit;
+//    
+//   function submit() {
+//        var name = document.getElementById("accountName").value;
+//        var balance = document.getElementById("deposit").value;
+//        
+//        addAcc = accountDetails();
+//        
+//        addAcc.accountCreated(name, balance);
+//        
+//        accountInfoList.push(addAcc);
+//        
+//        var showAccs = "";
+//        for (var i = 0; i < accountInfoList.length; i++) {
+//            showAccs += accountInfoList[i].print() + "\n";
+//        }
+//        textArea.value = showAccs;
+//        form.reset();
+//        return false;
+//        
+//    }
+//};
+
+
+var accountModule = (function () {
+
+    return {
+        createAccount: function (accountName, deposit) {
+            this.accountName = accountName;
+            this.deposit = deposit;
+        },
+        print : function(){
+            return "Account name : " + this.accountName + "    Balance : " + this.deposit;
+        }
+    };
 });
 
 
 
-windows.onload = function(){
-    
-    //document.getElementById("shw").onclick = rudyTimer();
-};
+    var form = document.getElementById("form");
+    var textArea = document.getElementById("textArea");
+    var accountInfoList = [];
+    var addAcc;
 
+    form.onsubmit = submit;
+
+    function submit() {
+        var name = document.getElementById("accountName").value;
+        var balance = document.getElementById("deposit").value;
+
+        addAcc = accountModule();
+        addAcc.createAccount(name , balance);
+        accountInfoList.push(addAcc);
+
+        var showAccs = "";
+        for (var i = 0; i < accountInfoList.length; i++) {
+            showAccs += accountInfoList[i].print() + "\n";
+        }
+        textArea.value = showAccs;
+        form.reset();
+        return false;
+    }
+};
 
